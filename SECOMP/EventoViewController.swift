6248d62110747a9c61e24a9d0dcbc9fec4f6c709
felipequecole <felipequecole@msn.com>
2016-08-16 14:43:53 -0300

    Atualizacoes na pagina do palestrante - bordas

--- SECOMP/EventoViewController.swift
+++ SECOMP/EventoViewController.swift
@@ -14,26 +14,42 @@ class EventoViewController: UIViewController {
     var viasegue = ""
     
     
+    @IBOutlet weak var imagePalestrante: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         label.text = viasegue
+        imagePalestrante.layer.borderWidth = 1.75
+        imagePalestrante.layer.borderColor = UIColor(colorLiteralRed: 0.9607843, green: 0.9607843, blue: 0.9607843, alpha: 1).CGColor
+        imagePalestrante.layer.cornerRadius = 10
+        imagePalestrante.clipsToBounds = true
+        
     }
-
+    
+    override func viewWillAppear(animated: Bool) {
+        //prepara o que vai aparecer
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        //depois que apareceu, o que acontece
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
