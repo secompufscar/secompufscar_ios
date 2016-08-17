c304ae08cd23f4afeb7b4897fd6ceb752ea3964b
felipequecole <felipequecole@msn.com>
2016-08-17 16:27:08 -0300

    Atualizações na orientacao do texto das views

---
+++ SECOMP/SobreDevViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SobreDevViewController.swift
+//  SECOMP
+//
+//  Created by Student on 8/17/16.
+//  Copyright © 2016 secompufscar. All rights reserved.
+//
+
+import UIKit
+
+class SobreDevViewController: UIViewController {
+
+    @IBOutlet weak var aboutDev: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        aboutDev.scrollRangeToVisible(NSMakeRange(0, 0))
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SECOMP/SobresViewController.swift
@@ -0,0 +1,42 @@
+//
+//  SobresViewController.swift
+//  SECOMP
+//
+//  Created by Student on 8/17/16.
+//  Copyright © 2016 secompufscar. All rights reserved.
+//
+
+import UIKit
+
+class SobresViewController: UIViewController {
+
+    
+    @IBOutlet weak var aboutSECOMP: UITextView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        aboutSECOMP.scrollRangeToVisible(NSMakeRange(0, 0))
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
