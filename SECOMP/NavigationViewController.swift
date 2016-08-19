c7edaeea948a5d9c5fe17ec285f7815adc471e7b
marcelodeolive1ra <marcelodeoliveira@outlook.com>
2016-08-19 00:38:54 -0300

    Atualizadas cores da navigation bar, atualizados ícones da tab bar, correção do layout da view Detalhes da atividade

---
+++ SECOMP/NavigationViewController.swift
@@ -0,0 +1,52 @@
+//
+//  NavigationViewController.swift
+//  SECOMP
+//
+//  Created by Marcelo de Oliveira da Silva on 8/19/16.
+//  Copyright © 2016 secompufscar. All rights reserved.
+//
+
+import UIKit
+
+class NavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationBar.tintColor = UIColor(netHex: 0x53B640)
+
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    convenience init(netHex:Int) {
+        self.init(red:(netHex >> 16) & 0xff, green:(netHex >> 8) & 0xff, blue:netHex & 0xff)
+    }
+}
